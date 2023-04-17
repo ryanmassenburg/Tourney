b8f4a903b46ad9f1204fefafefcb37a6708dfb68
chulleyboy <ryanmmassenburg@gmail.com>
2023-04-17 14:47:00 -0700

    fixed heroku deployment?

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const db = require('./config/connection');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/tourney_db');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
