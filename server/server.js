aabf7eeb9cc286699056ecbd92824149f35b498f
Ryan <ryanmmassenburg@gmail.com>
2023-04-17 18:57:06 -0700

    fixed server.js

--- server/server.js
+++ server/server.js
@@ -9,19 +9,14 @@ const db = require('./config/connection');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/tourney_db');
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+app.use(express.static(path.join(__dirname, '../client/build/')));
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
